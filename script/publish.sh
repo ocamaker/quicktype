d89c99428f203302eb87391fbb381ac76a630525
Vitor Pinto <vi.deap@gmail.com>
2026-01-21 09:59:40 -0300

    Publish version 23.2.7 [skip ci]

--- script/publish.sh
+++ script/publish.sh
@@ -2,40 +2,48 @@
 
 set -e
 
-./script/patch-npm-version.ts
+# ./script/patch-npm-version.ts
 
 VERSION=$(jq -r '.version' package.json )
-npm version $VERSION --workspaces --force
+# npm version $VERSION --workspaces --force
 
-# Publish core
-pushd packages/quicktype-core
-npm publish --access public
-popd
+# # Publish core
+# pushd packages/quicktype-core
+# npm publish --access public
+# popd
 
-# Publish typescript input
-pushd packages/quicktype-typescript-input
-jq --arg version $VERSION \
-    '.dependencies."@ocamaker/quicktype-core" = $version' \
-    package.json > package.1.json
-mv package.1.json package.json
-npm publish --access public
-popd
+# # Publish typescript input
+# pushd packages/quicktype-typescript-input
+# jq --arg version $VERSION \
+#     '.dependencies."@ocamaker/quicktype-core" = $version' \
+#     package.json > package.1.json
+# mv package.1.json package.json
+# npm publish --access public
+# popd
 
-# Publish graphql input
-pushd packages/quicktype-graphql-input
-jq --arg version $VERSION \
-    '.dependencies."@ocamaker/quicktype-core" = $version' \
-    package.json > package.1.json
-mv package.1.json package.json
-npm publish --access public
-popd
+# # Publish graphql input
+# pushd packages/quicktype-graphql-input
+# jq --arg version $VERSION \
+#     '.dependencies."@ocamaker/quicktype-core" = $version' \
+#     package.json > package.1.json
+# mv package.1.json package.json
+# npm publish --access public
+# popd
 
-# Publish quicktype
-jq --arg version $VERSION \
-    '.dependencies."@ocamaker/quicktype-core" = $version | .dependencies."@ocamaker/quicktype-graphql-input" = $version | .dependencies."@ocamaker/quicktype-typescript-input" = $version' \
-    package.json > package.1.json
-mv package.1.json package.json
-npm publish --access public
+# # Publish quicktype
+# jq --arg version $VERSION \
+#     '.dependencies."@ocamaker/quicktype-core" = $version | .dependencies."@ocamaker/quicktype-graphql-input" = $version | .dependencies."@ocamaker/quicktype-typescript-input" = $version' \
+#     package.json > package.1.json
+# mv package.1.json package.json
+# npm publish --access public
+
+# Commit the version bump
+git add .
+git commit -m "Publish version $VERSION [skip ci]"
+git push
+git tag "v$VERSION"
+git push origin main --tags
+
 
 
 # SKIPPING -> Won't Publish vscode extension
