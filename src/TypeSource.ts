6821fa544314bc9a255ec0ddcc6bdaf57068bed2
Vitor Pinto <vi.deap@gmail.com>
2026-01-21 09:43:56 -0300

    updated packages to ocamaker

--- src/TypeSource.ts
+++ src/TypeSource.ts
@@ -1,18 +1,18 @@
 import type { Readable } from "readable-stream";
 
 import type { JSONSchemaSourceData, JSONSourceData } from "quicktype-core";
-import type { GraphQLSourceData } from "quicktype-graphql-input";
+import type { GraphQLSourceData } from "@ocamaker/quicktype-graphql-input";
 
 export interface JSONTypeSource extends JSONSourceData<Readable> {
-    kind: "json";
+  kind: "json";
 }
 
 export interface SchemaTypeSource extends JSONSchemaSourceData {
-    kind: "schema";
+  kind: "schema";
 }
 
 export interface GraphQLTypeSource extends GraphQLSourceData {
-    kind: "graphql";
+  kind: "graphql";
 }
 
 export type TypeSource = GraphQLTypeSource | JSONTypeSource | SchemaTypeSource;
